2dd8679e20d0e6e6e9ca68bbd018f57603abb7ee
Idba1 <moniraislamself@gmail.com>
2024-03-08 12:08:08 +0600

    css style and dynamic property change

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,62 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>vite + react</h1>
+      <Student></Student>
+      <Student2></Student2>
+      <Student3 grad="7" score = "2.83"></Student3>
     </>
+  )
+}
+
+function Student (){
+  const studentStyle = {
+    margin: '20px',
+    padding: '20px',
+    border: '2px solid purple',
+    borderRadius: '20px'
+  }
+  const name = "Sakib";
+  const age = 15;
+  const cls = 9;
+  const myFriend = {
+    name : 'Jalil',
+    cls: 9,
+    age: 15,
+  }
+  return(
+    <div style={studentStyle}>
+    <h1>About my Self</h1>
+    <p>hello everyone. i'm {name}, i'm {age} years old. i read in class {cls}. my friend name is {myFriend.name}. now he is {myFriend.age} years old. and he read in class {cls}.</p>
+    </div>
+  )
+}
+
+
+function Student2() {
+  return (
+    <div className='student'>
+      <h3>This is a student</h3>
+      <p>Class:</p>
+      <p>score:</p>
+    </div>
+  )
+}
+
+
+
+function Student3(props) {
+  return (
+    <div style={{
+      margin: '20px',
+      padding: '20px',
+      border: '2px solid purple',
+      borderRadius: '20px'
+    }}>
+      <h3>This is a student</h3>
+      <p>Class: {props.grad}</p>
+      <p>score: {props.score}</p>
+    </div>
   )
 }
 
